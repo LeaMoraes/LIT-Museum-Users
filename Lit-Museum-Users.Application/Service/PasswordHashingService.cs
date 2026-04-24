10d58412853fa846cf62f68a0a0afd8e6216b524
LeaMoraes <leandroms.sr1708@gmail.com>
2026-04-24 18:03:26 -0300

    ADD: PasswordHashingService

---
+++ Lit-Museum-Users.Application/Service/PasswordHashingService.cs
@@ -0,0 +1,28 @@
+﻿using Lit_Museum_Users.Application.IService;
+
+namespace Lit_Museum_Users.Application.Service
+{
+    public class PasswordHashingService : IPasswordHashingService
+    {
+
+        public PasswordHashingService()
+        {
+        }
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            try
+            {
+                return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+            }
+            catch (BCrypt.Net.SaltParseException)
+            {
+                return false;
+            }
+        }
+    }
+}
--- Lit-Museum-Users.Tests/obj/Debug/net8.0/Lit-Museum-Users.Tests.AssemblyInfo.cs
+++ Lit-Museum-Users.Tests/obj/Debug/net8.0/Lit-Museum-Users.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lit-Museum-Users.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a239bb86eabcbd695aeb1cf15dc53b380c576e7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d75636979a96a2b7adb9abeef1295cb73f27e86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lit-Museum-Users.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lit-Museum-Users.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
